e0c4f9473430386aaa17c8e45b617e2fd579dff9
quanuw <quanuw@gmail.com>
2019-06-22 12:20:25 -0700

    Initial commit

---
+++ src/test/java/com/springboot/microservice/forex/springbootmicroserviceforexservice/SpringbootMicroserviceForexServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.microservice.forex.springbootmicroserviceforexservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMicroserviceForexServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
