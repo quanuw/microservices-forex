c36e5ca7e2d20e6733fd650e163164636d24d328
quanuw <quanuw@gmail.com>
2019-06-22 14:07:40 -0700

    Change root directory of project

---
+++ springboot-microservice-forex-service/src/main/java/com/springboot/microservice/forex/springbootmicroserviceforexservice/SpringbootMicroserviceForexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.forex.springbootmicroserviceforexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceForexServiceApplication.class, args);
+	}
+
+}
